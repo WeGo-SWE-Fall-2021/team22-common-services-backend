bb64daa5b75b5f8d413b9939d9f935d8ec985e33
Erik Bautista <ebautist@stedwards.edu>
2021-03-23 17:06:27 +0000

    Added mongo db password to a  file.

--- MongoUtils.py
+++ MongoUtils.py
@@ -1,19 +1,18 @@
 # Small utils for mongo db
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+mongo_secret = os.getenv("MONGO_SECRET")
 
 def checkUserInDatabase(self):
     return True
 
 
 def initMongoFromCloud(cloud):
-    if cloud == 'supply':
-        return MongoClient('localhost:27017',
-                           username="developer",
-                           password="team22_developer",
-                           authSource="team22_supply")
-    elif cloud == 'demand':
-        return MongoClient('localhost:27017',
-                           username="developer",
-                           password="team22_developer",
-                           authSource="team22_demand")
+    return MongoClient('localhost:27017',
+                       username="developer",
+                       password=f"{mongo_secret}",
+                       authSource=f"team22_{cloud}")
