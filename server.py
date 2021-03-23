cabd4b8fb17cbc16fecfc72655c983865daf0c4b
ErrorErrorError <erikbautista15@gmail.com>
2021-03-23 02:02:34 -0500

    Added response body on requests to avoid issues on fron end

--- server.py
+++ server.py
@@ -23,10 +23,13 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         cloud = postData['cloud']
         client = initMongoFromCloud(cloud)
         db = client['team22_' + cloud]
+        responseBody = {
+            'status': 'failed',
+            'message': 'Request not found'
+        }
 
         # receiving registration requests and writing their data to the database
         if '/register' in path:
-            status = 401  # HTTP Request: Unauthorized
             # TODO: add email address soon, but as of now username is fine
             registeredUsernameCount = db.user.count_documents({"username": postData["username"]})
             if registeredUsernameCount == 0:
@@ -35,17 +38,38 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 db.user.insert_one(user.__dict__)
 
                 status = 201  # HTTP Request: Created, Only if the user was registered
+                responseBody = {
+                    'status': 'success',
+                    'message': 'Successfully registered user.'
+                }
+            else:
+                status = 401  # HTTP Request: Unauthorized
+                responseBody = {
+                    'status': 'failed',
+                    'message': 'There is already a user registered with the username'  # TODO: Distinguish similarities
+                }
         elif '/login' in path:
             # TODO hash + salt password to see if that password is found
             validCredentials = db.user.count_documents({"username": postData["username"],
                                                         "password": postData["password"]})
             if validCredentials > 0:
                 status = 200
+                responseBody = {
+                    'status': 'success',
+                    'message': 'Successfully logged in.',
+                    'authentication': ''  # TODO Add Authentication token
+                }
             else:
                 status = 401
+                responseBody = {
+                    'status': 'failed',
+                    'message': 'Failed to find user with provided information'
+                }
         self.send_response(status)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
+        responseString = json.dumps(responseBody).encode('utf-8')
+        self.wfile.write(responseString)
         client.close()
 
     def do_GET(self):
